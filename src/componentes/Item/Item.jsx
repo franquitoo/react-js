b16b7ec3764c689e1a301759cae8f1650ff68f7c
franky <francoferreyrab@gmail.com>
2023-05-16 16:05:31 -0300

    asincronia

--- src/componentes/Item/Item.jsx
+++ src/componentes/Item/Item.jsx
@@ -4,18 +4,18 @@ import Button from "../Button/Button";
 import ItemCount from "../itemCount/ItemCount";
 import Itemlistcontainer from "../itemlistcontainer/Itemlistcontainer";
 
-function Item({}){
+function Item({title,img,precio,categoria}){
   return (
     <div className="item-card">
       <div className="item-card_header">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className="item-card_img">
-        <img src={props.img} alt="imagen"></img>
+        <img src={img} alt="imagen"></img>
       </div>
       <div className="item-card_detail">
-        <h4>$ {props.precio}</h4>
-        <small>{props.categoria}</small>
+        <h4>${precio}</h4>
+        <small>{categoria}</small>
       </div>
       <div>
          <Button color="purple">Ver detalle</Button>
--- src/componentes/itemlistcontainer/Itemlistcontainer.jsx
+++ src/componentes/itemlistcontainer/Itemlistcontainer.jsx
@@ -1,9 +1,26 @@
 import productos from "../../data/productos";
+import { useEffect, useState } from "react";
+
+function getData(){
+  return new Promise(resolve =>{
+    setTimeout(() => {resolve(productos)},2000)
+  })
+}
 
 function Itemlistcontainer() {
+
+let [products, setProducts] = useState([]);
+
+useEffect( () => {
+  getData().then((respuesta) => {
+    console.log("llegaron los productos",respuesta);
+    setProducts(respuesta);
+ });
+}, [])
+
   return (
     <div>
-      Itemlistcontainer{productos.map( (ItemInArray) => (
+      Itemlistcontainer{products.map( (ItemInArray) => (
        <Item 
         key={ItemInArray.id}
         id={ItemInArray.id}
